81a04dd412c18c0d048cf32f3748162e2938b891
hieu313 <hieunm3103@gmail.com>
2023-09-12 22:20:46 +0700

    thêm middleware cho group trong route

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,13 @@ use Illuminate\Http\Request;
 */
 Route::get('/', function (){
     return view('welcome');
-});
-Route::get('product/{id}', function (string $id){
-    return 'ID sản phẩm: '.$id;
-    /**
-     * nếu trong welcome.blade.php:
-     * dùng route('san-pham', ['id'=>1]) thì không bắt buộc phải có ?
-    */
-})->name('san-pham');
-Route::prefix('group')->group(function (){
+})->name('home');
+Route::prefix('group')->middleware('checkadminlogin')->group(function (){
     Route::get('add', function (){
         return 'thêm group';
     })->name('group.add') ;
+    /**
+     * khi dùng return redirect(route('home')) thì khi truy cập bất cứ cái nào trong group thì sẽ redirect đến route('home')
+     * http://localhost:8000/group/add thì lúc đó sẽ truy cập vào http://localhost:8000/
+    */
 });
