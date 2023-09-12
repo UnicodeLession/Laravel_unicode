5cf2890429d6159b5768a48253503628de1828b9
hieu313 <hieunm3103@gmail.com>
2023-09-12 21:14:04 +0700

    dùng name trong route

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,14 @@ use Illuminate\Http\Request;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('name/{name}/{age}', function (string $name, $age) {
-    return 'Tên của bạn là: '.$name.'. Tuổi: '.$age;
-    /**
-     * khi đó : http://localhost:8000/name/Nguyen-Minh-Hieu/18
-     * sẽ render ra : Tên của bạn là: Nguyen-Minh-Hieu. Tuổi: 18
-     *
-     * nếu : http://localhost:8000/name/
-     * thì sẽ bị 404
-     */
-})->where('name', '[a-zA-Z-]+')->where('age', '[\d]+');
-Route::get('web/{slug}-{id}.html',function ($id,$slug){
-    $content = 'id: '.$id.'<br />';
-    $content.= 'slug: '.$slug;
-    return $content;
-    /**
-     * * http://localhost:8000/web/co-thu-4652302.html
-     * khi đó slug: co | id: thu-4652302
-     *
-     * khi có nhiều param thì tham số bên trong url và bên trong funtion
-     * + có thể khác nhau về vị trí và tên
-     * + bắt buộc số lượng param của cả 2 phải bằng nhau
-     * + vị trí params bên trong funtion sẽ có giá trị tương ứng với vị trí bên trong url
-    */
-})->where(
-    [
-        'slug'=>'[a-z-]+',
-        'id'=>'[\d]+',
-    ]
-);
-
+Route::get('/', function (){
+    return view('welcome');
+});
+Route::get('product', function (){
+    return 'Danh sách sản phẩm';
+})->name('san-pham');
+Route::prefix('group')->group(function (){
+    Route::get('add', function (){
+        return 'thêm group';
+    })->name('group.add') ;
+});
