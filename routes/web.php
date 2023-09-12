f9978c36f3d3459b4ba01aab65fb8fda43bd4a71
hieu313 <hieunm3103@gmail.com>
2023-09-12 20:48:29 +0700

    params trong url

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,28 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::prefix('/group')->group(function (){
-    Route::view('/', 'welcome', ['name'=>'render welcome blade template trong group']);
-    Route::get('/add', function (){
-        return 'đã truy cập vào /group/add';
-    });
+Route::get('name/{name?}', function (string $name) {
+    return 'Tên của bạn là: '.$name;
+    /**
+     * khi đó : http://localhost:8000/name/Nguyen-Minh-Hieu
+     * sẽ render ra : Tên của bạn là: Nguyen-Minh-Hieu
+     *
+     * nếu : http://localhost:8000/name/
+     * thì sẽ bị 404
+     */
 });
+Route::get('web/{slug}-{id}.html',function ($id,$slug){
+    $content = 'id: '.$id.'<br />';
+    $content.= 'slug: '.$slug;
+    return $content;
+    /**
+     * * http://localhost:8000/web/co-thu-4652302.html
+     * khi đó slug: co | id: thu-4652302
+     *
+     * khi có nhiều param thì tham số bên trong url và bên trong funtion
+     * + có thể khác nhau về vị trí và tên
+     * + bắt buộc số lượng param của cả 2 phải bằng nhau
+     * + vị trí params bên trong funtion sẽ có giá trị tương ứng với vị trí bên trong url
+    */
+});
+
