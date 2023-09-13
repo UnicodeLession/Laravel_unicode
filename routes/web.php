484de24663c0ba494e180a9db415d926112223ef
hieu313 <hieunm3103@gmail.com>
2023-09-13 09:13:52 +0700

    sử dụng Controller để lấy route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,15 +14,11 @@ use Illuminate\Http\Request;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', function (){
-    return view('welcome');
-})->name('home');
-Route::prefix('group')->middleware('checkadminlogin')->group(function (){
-    Route::get('add', function (){
-        return 'thêm group';
-    })->name('group.add') ;
-    /**
-     * khi dùng return redirect(route('home')) thì khi truy cập bất cứ cái nào trong group thì sẽ redirect đến route('home')
-     * http://localhost:8000/group/add thì lúc đó sẽ truy cập vào http://localhost:8000/
-    */
-});
+// Dùng Controller
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/news',
+    'App\Http\Controllers\HomeController@getNews')->name('news');
+Route::get('/products',
+    'HomeController@getProducts')
+    ->name('products');
+
