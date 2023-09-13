9ea982593c07b727f06a764883a2696251c26321
hieu313 <hieunm3103@gmail.com>
2023-09-13 22:17:46 +0700

    tạo api products controller

---
+++ app/Http/Controllers/Api/ApiProductsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiProductsController extends Controller
+{
+    //
+    function getAllProducts()
+    {
+        return 'All Products';
+    }
+}
