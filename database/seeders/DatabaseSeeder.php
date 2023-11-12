3212c53896932c43dd899b4fd63fe69878468b37
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-11-12 15:19:12 +0700

    up dữ liệu lên db với seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+// do khi chạy câu lệnh trong terminal với seed thì chỉ chạy ở file này
+// nên bất cứ class nào được use ở các seeder khác cũng phải use ở đây
 
+//cách 1: `php artisan db:seed --class=UsersSeeder`
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Cách 2: gọi câu lệnh tổng hợp `php artisan db:seed`
+        $this->call(UsersSeeder::class);
+        $this->call(GroupsSeeder::class);
     }
 }
