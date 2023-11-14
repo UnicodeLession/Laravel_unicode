118853e7899ef08411ecd104e5197fb3cd1d440f
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-11-14 12:27:05 +0700

    thiết lập email và cấu hình forgot and reset password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,7 +59,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'min:5', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:1'],
+            'password' => ['required', 'string', 'min:6'],
             'password_confirmation' => ['required', 'same:password'],
         ],[
             'required'=>'Please enter :attribute ',
