949b8a49f7ed69fbda231e0c71c67d5dd22c4219
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-11-16 22:13:53 +0700

    sau khi logout thì redirect...

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -40,6 +41,24 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+// sau khi logout
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/login');
+    }
+
     protected function validateLogin(Request $request)
     {
         $request->validate([
