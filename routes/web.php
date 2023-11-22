d731799a62c2913dcccfda49f8eb7f6c9a366432
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-11-22 13:16:09 +0700

    xong dùng login bằng Google

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Laravel\Socialite\Facades\Socialite;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * Mỗi chỉnh sửa gì xong thì phải làm theo 3 bước dưới
+        git add .
+        git commit -am "make it better"
+        git push heroku master
+*/
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/auth/google', function () {
+    return Socialite::driver('google')->redirect();
+});
+Route::get('/auth/google/callback', function () {
+    $user = Socialite::driver('google')->user();
+    echo $user->getAvatar() ."<br />";
+    echo $user->getName() ."<br />";
+    echo $user->getEmail();
 });
