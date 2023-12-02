69a180bed325a83f6174d2b4f098417506ebfc44
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-12-02 16:12:45 +0700

    reset project - start Task Schedule

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UsersController;
-use App\Models\User;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,12 +16,4 @@ use App\Models\User;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-Route::prefix('users')->name('users')->group(function (){
-    Route::get('/', [UsersController::class, 'index'])->name('index');
-    Route::get('/{user}', [UsersController::class, 'detail'])->name('detail');
-    Route::post('/', [UsersController::class, 'create'])->name('create');
-    Route::put('/{user}', [UsersController::class, 'update'])->name('update_put');
-    Route::patch('/{user}', [UsersController::class, 'update'])->name('update_patch');
-    Route::delete('/{user}', [UsersController::class, 'delete'])->name('delete');
 });
