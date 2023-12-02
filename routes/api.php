703127077d824baf1b92431436c1340fe8b6b8a1
hieu313 <106041165+hieu313@users.noreply.github.com>
2023-12-02 15:51:53 +0700

    RESTful API CRUD

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\UsersController;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::prefix('users')->name('users')->group(function (){
+    Route::get('/', [UsersController::class, 'index'])->name('index');
+    Route::get('/{user}', [UsersController::class, 'detail'])->name('detail');
+    Route::post('/', [UsersController::class, 'create'])->name('create');
+    Route::put('/{user}', [UsersController::class, 'update'])->name('update_put');
+    Route::patch('/{user}', [UsersController::class, 'update'])->name('update_patch');
+    Route::delete('/{user}', [UsersController::class, 'delete'])->name('delete');
 });
